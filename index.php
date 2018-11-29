e7ad7b0f85e21289260b9e0c635297d7c6557c07
GoStars <gostars111@gmail.com>
2018-11-29 11:42:35 +0200

    Code optimization

--- index.php
+++ index.php
@@ -1,57 +1,32 @@
 <?php
-    // Change phone field
+    
     // Open input.csv file for read
     $input_handler = fopen('data/input.csv', 'r');
-    // Open changedphone.csv file for write
-    $changed_phone = fopen('data/changedphone.csv', 'w');
+    // Open output.csv file for write
+    $output = fopen('data/output.csv', 'w');
+
     // Get .scv file
     while (false !== ($line_of_text = fgetcsv($input_handler))) {
+        // Change phone field
         // Remove everything exept numbers from phone field
         $replaced_phone = str_replace(['+', '-', '(', ')', ' '], '', $line_of_text[6]);
         // Replace existing data with changed one
         $final_phone = str_replace($line_of_text[6], $replaced_phone, $line_of_text);
 
-        // Save changed data in new file
-        fputcsv($changed_phone, $final_phone);
-    }
-    // Close files
-    fclose($input_handler);
-    fclose($changed_phone);
-
-    // Change date field
-    $changed_phone_handler = fopen('data/changedphone.csv', 'r');
-    $changed_date = fopen('data/changeddate.csv', 'w');
-
-    while (false !== ($line_of_text = fgetcsv($changed_phone_handler))) {
         // Change format of a date 
         // Convert string from file to date format
-        $my_date = date("d-m-Y", strtotime($line_of_text[8]));
+        $my_date = date("d-m-Y", strtotime($final_phone[8]));
         $replaced_date = str_replace('-', '.', $my_date);
-        $final_date = str_replace($line_of_text[8], $replaced_date, $line_of_text);
+        $final_date = str_replace($final_phone[8], $replaced_date, $final_phone);
 
-        // Save changed data in new file
-        fputcsv($changed_date, $final_date);
-    }
-    // Close files
-    fclose($changed_phone_handler);
-    fclose($changed_date);
-
-    // Fixing birthday filed not showing properly
-    $changed_date_handler = fopen('data/changeddate.csv', 'r');
-    $output = fopen('data/output.csv', 'w');
-
-    while (false !== ($line_of_text = fgetcsv($changed_date_handler))) {
-        $replaced = str_replace('01.01.1970', 'birthday', $line_of_text[8]);
-        $final = str_replace($line_of_text[8], $replaced, $line_of_text);
+        // Fixing birthday filed not showing properly
+        $replaced = str_replace('01.01.1970', 'birthday', $final_date[8]);
+        $final = str_replace($final_date[8], $replaced, $final_date);
 
         // Save changed data in new file
         fputcsv($output, $final);
     }
+
     // Close files
-    fclose($changed_date_handler);
+    fclose($input_handler);
     fclose($output);
-    // Remove not needed files
-    unlink('data/changedphone.csv');
-    unlink('data/changeddate.csv');
-
-
